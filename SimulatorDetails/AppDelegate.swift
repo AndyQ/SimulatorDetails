0a184c59325351bfafb5d7f4a73fb52af5515b7e
AndyQ <andy.qua@gmail.com>
2014-11-06 12:54:33 +0000

    Initial Commit

---
+++ SimulatorDetails/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimulatorDetails
+//
+//  Created by Andy Qua on 06/11/2014.
+//  Copyright (c) 2014 Andy Qua. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SimulatorDetails/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimulatorDetails
+//
+//  Created by Andy Qua on 06/11/2014.
+//  Copyright (c) 2014 Andy Qua. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
